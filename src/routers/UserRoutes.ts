8dc4c96b5e70d64bc55af4d933412a0611ad4ba0
Muhammad Nurkholiq <mnurkholiq114@gmail.com>
2023-10-14 19:25:29 +0700

    auth and user

---
+++ src/routers/UserRoutes.ts
@@ -0,0 +1,27 @@
+import BaseRouter from './BaseRouter';
+import Controller from '../controllers/User';
+import AuthGuard from '../middleware/authentication';
+import validator from '../middleware/validator';
+
+// export
+class UserRoutes extends BaseRouter {
+  public routes(): void {
+    // get
+    this.router.get('/', AuthGuard.checkAccessTokenAdmin, Controller.getAllUser);
+    this.router.get('/all', AuthGuard.checkAccessTokenAdmin, Controller.getAllUser);
+    this.router.get('/table', AuthGuard.checkAccessTokenSuperAdmin, Controller.getTableUser);
+    this.router.get('/:id', AuthGuard.checkAccessTokenSuperAdmin, Controller.getUser);
+
+    // post
+    this.router.post('/', AuthGuard.checkAccessTokenSuperAdmin, validator.validate, Controller.createUser);
+
+    // patch
+    this.router.patch('/:id', AuthGuard.checkAccessTokenSuperAdmin, validator.validate, Controller.updateUser);
+    this.router.patch('/changePassword/:id', AuthGuard.checkAccessTokenSuperAdmin, validator.validate, Controller.changePassword);
+
+    // delete
+    this.router.delete('/:id', AuthGuard.checkAccessTokenSuperAdmin, validator.validate, Controller.deleteUser);
+  }
+}
+
+export default new UserRoutes().router;
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,10 +3,17 @@ import BaseRouter from './BaseRouter';
 // test
 import PingRoutes from './PingRoutes';
 
+// main routes
+import UserRoutes from './UserRoutes';
+
 class Routers extends BaseRouter {
   public routes(): void {
+    // test
     this.router.use('/', PingRoutes);
     this.router.use('/ping', PingRoutes);
+
+    // main routes
+    this.router.use('/user', UserRoutes);
   }
 }
 
